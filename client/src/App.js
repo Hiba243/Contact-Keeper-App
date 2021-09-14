bda201a1efe9bb416c2138cd306d46dd42b9a254
Hiba243 <hibafatima24@gmail.com>
2021-09-14 19:49:23 +0530

    created context and global state

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
 import { Fragment } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import ContactState from '../src/context/contact/ContactState'
 
 const App = () => {
   return (
-    <Router>
-    <Fragment className="App">
-      <Navbar/>
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route exact path="/about" component={About}></Route>
-        </Switch>
-      </div>
-    </Fragment>
-    </Router>
+    <ContactState>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/about" component={About}></Route>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </ContactState>
   );
 }
 
---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,26 @@
+import {
+    GET_CONTACTS,
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER,
+    CONTACT_ERROR,
+    CLEAR_CONTACTS
+} from '../types';
+
+export default (state, action) => {
+    switch (action.type) {
+        case ADD_CONTACT:
+            return {
+                ...state,
+                contacts: [action.payload, ...state.contacts],
+                
+              };
+
+        default:
+            return state;
+    }
+};
